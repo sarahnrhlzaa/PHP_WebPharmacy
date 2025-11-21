8529630102ce4251803cb5fad4faa9ae71861473
sarahnrhlzaa <sarahnrhlza3rut@gmail.com>
2025-11-21 09:44:47 +0700

    nyoba deploy lagi

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
-<?php echo "test"; ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Situs Pages Berhasil!</title>
+</head>
+<body>
+    <h1>Halo Sarah, GitHub Pages berfungsi!</h1>
+    <p>Halaman ini tidak bisa menjalankan PHP, tetapi HTML sudah terbit.</p>
+</body>
+</html>
